e439a4e6eb851e768814b31457109836b6e0d35f
Fabricio Bravo <vn0de9e@M-C02D1G8DMD6M.local>
2021-04-22 01:05:40 -0400

    add health handler

--- src/main/java/api/infrastructure/web/handlers/HealthHandler.java
+++ src/main/java/api/infrastructure/web/handlers/HealthHandler.java
@@ -1,29 +1,18 @@
 package api.infrastructure.web.handlers;
-
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
 
 
-//import org.springframework.web.bind.annotation.PostMapping;
-
-
-//@Controller
 @RestController
-//@RequestMapping(path="/api",produces="application/json")
 @CrossOrigin(origins="*")
 public class HealthHandler {
 	
 	@GetMapping("/health")
-    public String getMessage() {
-        return "A brand new product";
+    public HashMap<String, String>  getMessage() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "UP");
+        return map;
     }
-//	@PostMapping(value = "/move")
-//	public void create(@RequestBody RoberPosition item) {  //ResponseEntity<?>
-//
-//		//return service.create(item).getResponse();
-//	}
-
 }
 
 
